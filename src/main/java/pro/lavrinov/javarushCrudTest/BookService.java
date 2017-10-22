cddad4051034ae3d5130e770b2d1106fc33acc94
Lavrinov <lavrinov39@gmail.com>
2017-10-22 16:09:55 +0200

    Add files via upload

---
+++ src/main/java/pro/lavrinov/javarushCrudTest/BookService.java
@@ -0,0 +1,13 @@
+package pro.lavrinov.javarushCrudTest.service;
+
+import pro.lavrinov.javarushCrudTest.model.Book;
+
+import java.util.List;
+
+public interface BookService {
+    public void addBook(Book book);
+    public void updateBook(Book book);
+    public void removeBook(int id);
+    public Book getBookById(int id);
+    public List<Book> listBooks();
+}
